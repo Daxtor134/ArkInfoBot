5061bd5f1233c18885da19d3a6737a8a6ca449e8
Daxtor134 <jamesrw35@gmail.com>
2020-05-16 20:49:57 -0700

    Update index.js

--- index.js
+++ index.js
@@ -1,22 +1,68 @@
-// Import dependencies
 const discord = require('discord.js');
 const chalk = require('chalk');
-
-// Enviroment Variables
+const fs = require('fs');
 require('dotenv').config();
 
-// Create the Discord bot object.
-const client = new discord.Client();
+const prefix = process.env.PREFIX;
 
-client.once('ready', () => {
-    console.log(chalk.green(`Discord bot ${client.user.tag} is currently running.`));
+const client = new discord.Client();
+client.commands = new discord.Collection();
+
+const commandFiles = fs.readdirSync(`./commands`).filter(file => file.endsWith(`.js`));
+
+for (const file of commandFiles)
+{
+
+    const command = require(`./commands/${file}`);
+    client.commands.set(command.name, command);
+    console.log(chalk.yellow(`Loaded ${file}`));
+
+}
+
+client.once(`ready`, () => {
+
+    console.log(chalk.green(`${client.user.tag} is now currently running; type in the command "!help" to get information on commands!`));
+
 });
 
-client.on('message', message => {
-    if(message.content === 'ping')
+client.on(`message`, msg => {
+
+    if (!msg.content.startsWith(prefix) || msg.author.bot) return;
+
+    const args = msg.content.slice(prefix.length).split(/ +/);
+    const commandName = args.shift().toLowerCase();
+
+    const command = client.commands.get(commandName)
+        || client.commands.get(cmd => cmd.aliases && cmd.aliases.includes(commandName));
+
+    if (!command) return;
+
+    if (command.guildOnly && msg.channel.type !== `text`)
     {
-        message.reply('pong');
+        return msg.reply(`I can't exeucte that command inside DM's.`);
     }
+
+    if (command.args && !args.length)
+    {
+        let reply = `You didn't provide any arguments, ${msg.author}`;
+        if(command.usage)
+        {
+            reply += `\nThe proper usage would be: \`${prefix}${command.name} ${command.usage}\``;
+        }
+
+        return  msg.channel.send(reply);
+    }
+
+    try
+    {
+        command.execute(msg, args);
+    }
+    catch(e)
+    {
+        console.error(chalk.red(e));
+        msg.reply(`There was an error trying to process that command!`);
+    }
+
 });
 
 client.login(process.env.TOKEN);
