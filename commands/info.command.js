32fe0764ec89f2881cd602e953058e2bff9a324f
Daxtor134 <jamesrw35@gmail.com>
2020-05-17 22:02:15 -0700

    Create info.command.js

---
+++ commands/info.command.js
@@ -0,0 +1,11 @@
+module.exports = {
+      "name": "info",
+      "description": "This command will give info about the person specified in the env. settings.",
+      "args": true,
+      "usage": "<platform [YouTube, Twitch]>",
+      "guildOnly": true,
+      execute(msg, args)
+      {
+          // TODO: Write info command.
+      }
+  }
