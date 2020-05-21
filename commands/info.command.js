29a7ba72f27b8d4e2d9d1406802869e4687258fd
Daxtor134 <jamesrw35@gmail.com>
2020-05-21 16:08:09 -0700

    Update info.command.js

--- commands/info.command.js
+++ commands/info.command.js
@@ -1,5 +1,6 @@
 const Discord = require('discord.js');
 const Chalk = require('chalk');
+const channels = require('../config/channels.json');
 
 const log = console.log;
 
@@ -14,11 +15,45 @@ module.exports = {
       {
             if (args[0] === "--yt")
             {
-                  message.reply("Used YouTube info command.");
+                  // Send console message stating that a user used a command.
+                  log(Chalk.yellow(`@<${message.author.id}> Used a Command.`));
+
+                  // Create the Message Embed
+                  // @YouTubeMessageEmbed
+                  const YouTubeMessageEmbed = new Discord.MessageEmbed()
+                        .setColor(`#fc1c03`)
+                        .setTitle(`**${channels.YouTube.ChannelName}**`)
+                        .setDescription(`${channels.YouTube.ChannelDescription}`)
+                        .setThumbnail(`${channels.YouTube.ChannelIcon}`)
+                        .setAuthor(`ArkInfoBot`, `${channels.YouTube.ChannelIcon}`)
+                        .addField(`Channel Link`, `${channels.YouTube.ChannelLink}`)
+                        .addField(`Check out a video`, `${channels.YouTube.ChannelPreviewVideo}`)
+                        .setTimestamp()
+                        .setFooter(`ArkInfoBot`, `${channels.YouTube.ChannelIcon}`);
+
+                  // Send the Message Embed
+                  message.reply(YouTubeMessageEmbed);
             }
             if (args[0] === "--twitch")
             {
-                  message.reply("Used the Twitch info command.");
+                  // Send console message stating that a user used a command.
+                  log(Chalk.yellow(`@<${message.author.id}> Used a Command.`));
+
+                  // Create the Message Embed
+                  // @TwitchMessageEmbed
+                  const TwitchMessageEmbed = new Discord.MessageEmbed()
+                        .setColor(`#8403fc`)
+                        .setTitle(`**${channels.Twitch.ChannelName}**`)
+                        .setDescription(`${channels.Twitch.ChannelDescription}`)
+                        .setThumbnail(`${channels.Twitch.ChannelIcon}`)
+                        .setAuthor(`ArkInfoBot`, `${channels.Twitch.ChannelIcon}`)
+                        .addField(`Channel Link`, `${channels.Twitch.ChannelLink}`)
+                        .addField(`Check out a video`, `${channels.Twitch.ChannelPreviewVideo}`)
+                        .setTimestamp()
+                        .setFooter(`ArkInfoBot`, `${channels.Twitch.ChannelIcon}`);
+
+                  // Send the Message Embed
+                  message.reply(TwitchMessageEmbed);
             }
       }
 };
